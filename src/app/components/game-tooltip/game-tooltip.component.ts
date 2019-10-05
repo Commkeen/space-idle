db2b4e4e58a01804d48f8755362da6e1bd5f7e87
Commkeen <commkeen4000@gmail.com>
2019-10-05 15:26:45 -0700

    Tooltip uses model

--- src/app/components/game-tooltip/game-tooltip.component.ts
+++ src/app/components/game-tooltip/game-tooltip.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TooltipViewModel } from 'src/app/models/tooltipViewModel';
+import { ResourceCollection } from 'src/app/models/resource';
 
 @Component({
   selector: 'app-game-tooltip',
@@ -9,5 +10,14 @@ import { TooltipViewModel } from 'src/app/models/tooltipViewModel';
 export class GameTooltipComponent {
 
   @Input() tooltipModel: TooltipViewModel;
+
+  constructor() {
+    this.tooltipModel = new TooltipViewModel();
+    this.tooltipModel.name = 'Default Name';
+    this.tooltipModel.desc = 'Lorem ipsum dolor sit amet.  Lorem ipsum dolor sit amet.';
+    this.tooltipModel.costs = new ResourceCollection();
+    this.tooltipModel.costs.add('metal', 21);
+    this.tooltipModel.costs.add('silicates', 52);
+  }
 
 }
