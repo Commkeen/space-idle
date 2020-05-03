4c9aaf00a0f05db3a9b14f0597e9ca7a185dc854
Matt Everett <mceverett@cavernlore.com>
2020-05-02 22:28:49 -0700

    Tune down survey and drone hub scaling

--- src/app/staticData/regionDefinitions.ts
+++ src/app/staticData/regionDefinitions.ts
@@ -15,10 +15,10 @@ export class RegionDefinition {
 
   public description = '';
   public surveyUpgradeNeeded = '';
-  public surveyBaseCost: number = 10;
-  public surveyCostMultiplier: number = 2.2;
+  public surveyBaseCost: number = 25;
+  public surveyCostMultiplier: number = 1.8;
   public droneHubBaseCost: ResourceCollection = new ResourceCollection();
-  public droneHubCostMultiplier: number = 1.5;
+  public droneHubCostMultiplier: number = 1.6;
   public infrastructure: InfrastructureDefinition[] = [];
 
   constructor(
@@ -59,52 +59,35 @@ export class RegionDefinition {
 export const REGION_LIBRARY: RegionDefinition[] = [
   new RegionDefinition('Plains').setDescription('Rolling plains.')
     .setSurveyBaseCost(10)
-    .setSurveyCostMultiplier(2.3)
-    .addDroneHubCost('metal', 24)
-    .setDroneHubCostMultiplier(2.2),
+    .addDroneHubCost('metal', 24),
   new RegionDefinition('Hills').setDescription('Geographically complex region rich in minerals.')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
-  .addDroneHubCost('nanochips', 4)
-  .setDroneHubCostMultiplier(2.2),
+  .addDroneHubCost('nanochips', 4),
   new RegionDefinition('Mountain').setDescription('Difficult to traverse, but can contain many valuable materials.')
   .setSurveyUpgradeNeeded('Mountain Survey')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
   .addDroneHubCost('duranium', 25)
   .setDroneHubCostMultiplier(2.2),
   new RegionDefinition('Coast').setDescription('Coast description.')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
-  .addDroneHubCost('metal', 2)
-  .setDroneHubCostMultiplier(2.2),
+  .addDroneHubCost('duranium', 25),
   new RegionDefinition('Forest').setDescription('Forest description.')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
-  .addDroneHubCost('metal', 2)
-  .setDroneHubCostMultiplier(2.2),
+  .addDroneHubCost('duranium', 25),
   new RegionDefinition('Ocean').setDescription('Ocean description.')
   .setSurveyUpgradeNeeded('Maritime Survey')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
-  .addDroneHubCost('metal', 2)
-  .setDroneHubCostMultiplier(2.2),
+  .addDroneHubCost('duranium', 25),
   new RegionDefinition('Ocean Floor').setDescription('Ocean Floor description.')
   .setSurveyUpgradeNeeded('Undersea Survey')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
-  .addDroneHubCost('metal', 2)
-  .setDroneHubCostMultiplier(2.2),
+  .addDroneHubCost('duranium', 25),
   new RegionDefinition('Desert').setDescription('Desert description.')
   .setSurveyUpgradeNeeded('Desert Survey')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
-  .addDroneHubCost('metal', 2)
-  .setDroneHubCostMultiplier(2.2),
+  .addDroneHubCost('duranium', 25),
   new RegionDefinition('Arctic').setDescription('Arctic description.')
   .setSurveyUpgradeNeeded('Arctic Survey')
   .setSurveyBaseCost(25)
-  .setSurveyCostMultiplier(2.4)
-  .addDroneHubCost('metal', 2)
-  .setDroneHubCostMultiplier(2.2),
+  .addDroneHubCost('duranium', 25),
 ];
